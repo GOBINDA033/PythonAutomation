6ea34f38eb94328cc2b20071093bbc37585f1670
Gobinda <114664223+GOBINDA033@users.noreply.github.com>
2025-11-12 08:08:08 +0530

    How to comment filter work on social media.

---
+++ Comment.py
@@ -0,0 +1,7 @@
+#SocialMediaCommentFilter
+comment=input("Enter your comment")
+if "stupid"in comment or"hate"in comment or "ididot" in comment:
+    print("Block this comment")
+else:
+    print("approved this comment")
+    
