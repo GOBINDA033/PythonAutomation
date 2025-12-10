de3c5bf8ac11b7349d29f484145c03126807c04f
Gobinda <114664223+GOBINDA033@users.noreply.github.com>
2025-12-10 20:51:55 +0530

    Add files via upload
    This is the basic of python ,include all basic concepts such as variable , Single-line comments, Multiple-line comments,Find Data Type and Type casting ect.

---
+++ BasicOfPython.py
@@ -0,0 +1,34 @@
+#This hash simbol is used for single line comments in python
+a=10
+b=20
+c=a+b
+print("The sum of a and b is:",c)
+
+""" This is a multi-Line comment in python
+You can use triple quotes to create multiple-line comments.
+""" 
+#By using type() function we can check the data type of a vriable
+num1="23"
+print(type(num1))
+
+num=10
+print(type(num))
+
+num2=10.5
+print(type(num2))
+
+#Now we are going to do type casting in python
+#Example of different type casting  
+
+x="20" #string to int
+a=int(x) #int to float 
+f=float(a) #float to string
+
+print(type(a))
+print(type(f))
+print(type(x))
+
+
+
+ 
+ 
