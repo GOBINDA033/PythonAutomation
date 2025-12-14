4947258ddf9106a0f138d444995593c2bb3e34fd
Gobinda <114664223+GOBINDA033@users.noreply.github.com>
2025-12-14 21:12:07 +0530

    Add files via upload

---
+++ String.py
@@ -0,0 +1,31 @@
+#In string  python using indexing
+string="Hey Gobinda Bro"
+print(string[2])
+
+#uppercase
+print(string.upper())
+
+#lowercase
+print(string.lower())
+
+#stripping 
+string2=" Hey Gobinda Bro"
+print(string2.strip())
+
+# length of string
+print(len(string))
+
+# replacing string
+print(string.replace("Bro","Bhai"))
+
+#stratswith
+string4="Gobinda.pdf"
+print(string4.startswith("Gobinda"))
+
+#endswith
+print(string4.endswith(".pdf"))
+
+#counting string
+string5="hy hy by my hy by"
+print(string5.count("by"))
+
