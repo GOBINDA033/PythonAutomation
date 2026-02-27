9884d37a1dcea103761b6ea80b3da6ca3d492b3b
Gobinda <114664223+GOBINDA033@users.noreply.github.com>
2026-02-27 12:54:45 +0530

    Update Graph.py

--- Graph.py
+++ Graph.py
@@ -6,3 +6,24 @@ plt.title("Bar line Graph")
 plt.xlabel("x-axis")
 plt.ylabel("y-axis")
 plt.show() 
+
+
+
+
+
+#import matplotlib.pyplot as plt
+
+# Data
+students = ["Ram", "Shyam", "Mohan", "Ravi"]
+marks = [85, 90, 78, 88]
+
+# Bar graph
+plt.bar(students, marks)
+
+# Title aur labels
+plt.title("Students Marks")
+plt.xlabel("Students")
+plt.ylabel("Marks")
+
+# Show graph
+plt.show()
